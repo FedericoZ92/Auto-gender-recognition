0ef104a78ef21431cbc4e934e7b2a4e8d7150965
Federico Zanetti <federico.zanetti@seitron.it>
2025-12-26 18:40:06 +0100

    chore: finalize directories

--- GENDER_Literature/alg4/ped_detector_RELEASE/ped_detector_RELEASE/ped-demo-fast-8x8x1.1/compute_feature_dim.m
+++
@@ -1,6 +0,0 @@
-function dim=compute_feature_dim(ww,hh,nori)
-   dim = zeros(length(ww),1);
-   for i = 1:length(ww)
-     dim(i) = nori*(size(ww{i},1)-1)*(size(ww{i},2)-1);
-   end
-end
--- GENDER_Literature/alg6/pedestrian-detection-master/cumsum2D.m
+++
@@ -1,5 +0,0 @@
-
-function CD = cumsum2D(D)
-   CD = zeros(size(D,1)+1,size(D,2)+1,size(D,3));
-   CD(2:end,2:end,:) = cumsum(cumsum(D,1),2);
-end
